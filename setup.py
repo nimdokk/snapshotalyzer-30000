9375443cfb628d17d7d22351e54e643cd0bb52f4
nimdokk@gmail.com <nimdokk@gmail.com>
2020-02-04 16:59:43 -0600

    Setup.py added to enable building

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='snapshotalyzer-30000',
+    version='0.1',
+    author="David Daspit",
+    author_email="nimdokk@gmail.com",
+    description="SnapshotAlyzer 30000 is a tool to manage AWS EC2 snapshots",
+    license="GPLv3+",
+    packages=['shotty'],
+    url="https://github.com/nimdokk/snapshotalyzer-30000",
+    install_requires=[
+        'click',
+        'boto3',
+    ],
+    entry_points='''
+        [console_scripts]
+        shotty=shotty.shotty:cli
+    ''',
+)
+
